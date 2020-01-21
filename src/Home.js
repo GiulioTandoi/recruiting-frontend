fd02a29d9053c2f80286868506b5d3ddf768c182
Giulio Tandoi <gtandoi@alecsandria.it>
2020-01-21 18:10:03 +0100

    Minor changes

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ class Home extends Component{
     
     constructor(props){
         super(props);
-        this.state={listaelementi :[], id : this.props.location.state.id};
+        this.state={listaelementi :[], id : this.props.location.state.id };
         // Quando passo più elementi nello state li passo come una lista e vi accedo semplicemente da state[idElementoLista]
         //console.log("L'id del selezionatore è " + this.props.location.state.id);
 
@@ -17,7 +17,9 @@ class Home extends Component{
 
     componentDidMount = () => {
 
-        if (this.props.location.state.searched === false){
+     
+
+        if (this.props.location.state.searched === false ){
         const apiUrl = "http://localhost:8080/home";  // bisogna cambiare il metodo in modo che faccia una post
         axios.get(apiUrl)
           .then(response => {
@@ -33,14 +35,15 @@ class Home extends Component{
           .catch(error => {
             console.log(error);
           });
+          
         }
     }
 
     componentDidUpdate = () => {
 
-        
+        var count =0;
 
-        if (this.props.location.state.searched === true){
+        if (this.props.location.state.searched === true && count ===0){
 
             const apiUrl = "http://localhost:8080/search";
             axios.get(apiUrl, 
@@ -60,12 +63,13 @@ class Home extends Component{
                 }else {
                     window.alert("La ricerca non ha prodotto risultati")
                 }
-                
+                this.setState({searched : false})
             }
             )
             .catch(error => {
                 console.log(error);
             });
+            count++;
         }
     }
 
